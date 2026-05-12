1f7350cf948a62f8d93b02ec23104874e3a31bb9
Jerome <jerome.guya@evsu.edu.ph>
2026-05-12 10:34:08 +0800

    Added logout

--- backend/movie-stream/src/routes/authRoutes.js
+++ backend/movie-stream/src/routes/authRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { register, login } from "../controllers/authController.js";
+import { register, login, logout } from "../controllers/authController.js";
 import { validateRequest } from "../middleware/validateMiddleware.js";
 import { registerSchema } from "../validator/registerValidator.js";
 import { ro } from "zod/v4/locales";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", validateRequest(registerSchema), register);
 router.post("/login", login);
+router.post("/logout", protect, logout);
 
 export default router;
