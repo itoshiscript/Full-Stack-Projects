71970168141e4a11404e2d665ed6f9a359166583
Jerome <jerome.guya@evsu.edu.ph>
2026-06-20 07:46:52 +0800

    Added QueryClientProvider and updated package.json

--- front-end/hotel-management-system/src/pages/Home.tsx
+++ front-end/hotel-management-system/src/pages/Home.tsx
@@ -210,7 +210,7 @@ export const Home = () => {
 
             {/* Footer */}
             <footer className="border-t border-gray-200 bg-white">
-                <div className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
+                <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                     <div className="flex flex-col items-center justify-between gap-4 sm:flex-row">
                         <div className="flex items-center gap-2">
                             <div className="flex h-7 w-7 items-center justify-center rounded-lg bg-gray-900 text-white">
